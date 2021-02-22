6e334e3728b6ff8e31a45502cfc79b3910cee1e8
Harvey2504 <atibsamal96@gmail.com>
2021-02-22 15:13:25 +0530

    adding new key

---
+++ Jenkinsfile1.groovy
@@ -0,0 +1,30 @@
+pipeline {
+    agent any
+    triggers{
+        pollSCM('* * * * *')
+    }
+    
+    tools{
+            maven 'maven-3'
+            jdk 'java11'
+    }
+    //Nothing Interesting Happening
+    stages {
+        stage('clean')
+        {
+            steps{
+            bat 'mvn clean'
+            }
+        }
+        stage('build && SonarQube analysis') {
+            steps {
+                withSonarQubeEnv('sonarqube2') {
+                    
+                        bat 'mvn package sonar:sonar'
+                    
+                }
+            }
+        }
+    }
+    
+}
